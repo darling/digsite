4ee407cfc8fea66d637dc1eaeeeaa210ce6d4c17
zackwcs <52152719+zackwcs@users.noreply.github.com>
2024-04-04 18:02:40 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -124,8 +124,5 @@ async fn main() -> Result<()> {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
-    // TODO: Remove this
-    let _digsite = DigSite {};
-
     Ok(())
 }
